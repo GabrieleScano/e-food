4fc2bab0cc793caa5d0ab0a7ddf5e32b42630698
gabri <gabrielescano.bcn@gmail.com>
2021-10-25 11:49:56 +0200

    Add MealSummaty

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,9 @@
+import classes from './MealsSummary.module.css'
+export const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious food, Delivered to You</h2>
+            <p>Choose your favourete meal from selection</p>
+        </section>
+    )
+}
