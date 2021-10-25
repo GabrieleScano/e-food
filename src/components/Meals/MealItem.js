0e435f007d69b3f4a45bf4c78d748f82c7614b57
gabri <gabrielescano.bcn@gmail.com>
2021-10-25 13:48:04 +0200

    Add input component

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css'
+import { MealItemForm } from './MealItemForm'
 
 export const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`
@@ -8,9 +9,9 @@ export const MealItem = (props) => {
                 <h3>{props.name}</h3>
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
-                <div>
-
-                </div>
+            </div>
+            <div>
+                <MealItemForm />
             </div>
         </li>
     )
