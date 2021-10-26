cc0d04e63fd7405b0a0dfa3c46bb40bd7f7d1811
gabri <gabrielescano.bcn@gmail.com>
2021-10-26 10:42:32 +0200

    Add Reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Cart } from "./components/Cart/Cart";
 import { Header } from "./components/Layout/Header";
-import { Meals } from "./components/Meals/Meals";
-import { CartProvider } from "./store/CartProvider";
+import { Meals } from './components/Meals/Meals'
+import { CartProvider } from './store/CartProvider'
 
 function App() {
 
