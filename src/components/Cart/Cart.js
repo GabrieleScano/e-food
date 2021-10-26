e0e7d47a67e5e741ee2c2dbe440bac671ce165e9
gabri <gabrielescano.bcn@gmail.com>
2021-10-26 15:06:30 +0200

    Animate button cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,13 +6,18 @@ import { CartItem } from './CartItem'
 
 export const Cart = (props) => {
 
-    const cartCtx =useContext(CartContext)
+    const cartCtx = useContext(CartContext)
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
     const hasItems = cartCtx.items.length > 0
 
-    const cartItemRemoveHandler = id => {}
-    const cartItemAddHandler =item => {}
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id)
+    }
+
+    const cartItemAddHandler = item => {
+        cartCtx.addItem(item)
+    }
 
     const cartItems = (
     <ul className={classes['cart-items']}>
