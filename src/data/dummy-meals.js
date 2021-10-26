2708ac76ba3274a75fd28fa3f149aca69e832a14
gabri <gabrielescano.bcn@gmail.com>
2021-10-27 00:10:06 +0200

    change mock data position

--- src/data/dummy-meals.js
+++ src/data/dummy-meals.js
@@ -1,19 +1,19 @@
 export const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
+  {
+    id: 'm2',
+    name: 'Pizza',
+    description: 'An Italian specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
     {
       id: 'm3',
-      name: 'Barbecue Burger',
+      name: 'Hamburger',
       description: 'American, raw, meaty',
       price: 12.99,
     },
